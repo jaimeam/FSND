a280f5e2f51d5953c5b896954a49db626102daf0
Jaime Adroher <jaime.adroher@gmail.com>
2020-03-06 08:24:45 +0100

    decouple models.py

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -5,7 +5,16 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
+from flask import (
+  Flask,
+  render_template,
+  request,
+  Response,
+  flash,
+  redirect,
+  url_for,
+  jsonify
+)
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -17,58 +26,14 @@ import sys
 import datetime
 from sqlalchemy.sql import func
 
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-
-migrate = Migrate(app,db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-  __tablename__ = 'venue'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, nullable=False, unique=True)
-  city = db.Column(db.String(120), nullable=False)
-  state = db.Column(db.String(120), nullable=False)
-  address = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  genres = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  seeking_talent = db.Column(db.Boolean, default=False)
-  seeking_description = db.Column(db.String(500))
-  website = db.Column(db.String(500))
-
-class Artist(db.Model):
-  __tablename__ = 'artist'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, nullable=False, unique=True)
-  city = db.Column(db.String(120), nullable=False)
-  state = db.Column(db.String(120), nullable=False)
-  phone = db.Column(db.String(120))
-  genres = db.Column(db.String(500))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  seeking_venue = db.Column(db.Boolean, default=False)
-  seeking_description = db.Column(db.String(500))
-  website = db.Column(db.String(500))
-
-class Show(db.Model):
-  __tablename__ = 'show'
-  id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id', ondelete='CASCADE'), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id', ondelete='CASCADE'), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False)
-  venue = db.relationship(Venue,backref='shows')
-  artist = db.relationship(Artist,backref='shows')
+#Import models and SQLAlchemy connection
+from models import (
+  Venue,
+  Artist,
+  Show,
+  app,
+  db
+)
 
 #----------------------------------------------------------------------------#
 # Filters.
